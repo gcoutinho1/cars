a76ebbb8235474a77f01c4b554d74c6a67c0263b
Guilherme Coutinho <guilhermecoutinho7777@gmail.com>
2021-11-22 14:40:05 -0300

    saving the user and the last tab he browsed in sharedPreferences

--- lib/pages/cars/home_page.dart
+++ lib/pages/cars/home_page.dart
@@ -2,6 +2,7 @@ import 'package:cars/drawer_list.dart';
 import 'package:cars/pages/cars/cars.dart';
 import 'package:cars/pages/cars/cars_api.dart';
 import 'package:cars/pages/cars/cars_listview.dart';
+import 'package:cars/utils/prefs.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -9,28 +10,57 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin<HomePage>{
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin<HomePage> {
+
+  TabController _tabController;
+
+  @override
+  void initState() {
+
+    super.initState();
+    _tabController = TabController(length: 3, vsync: this);
+    // _tabController.index = 1;
+    Future<int> future = Prefs.getInt("tabIdx");
+    future.then((int idx) => _tabController.index = idx);
+
+    _tabController.addListener(() {
+      print("Tab ${_tabController.index}");
+
+      Prefs.setInt("tabIdx", _tabController.index);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Carros"),
-          bottom: TabBar(tabs: [
-            Tab(text: "Clássicos",),
-            Tab(text: "Esportivos",),
-            Tab(text: "Luxo",),
-          ],),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Carros"),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              text: "Clássicos",
+            ),
+            Tab(
+              text: "Esportivos",
+            ),
+            Tab(
+              text: "Luxo",
+            ),
+          ],
         ),
-        body: TabBarView(children: [
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
           CarsListView(TipoCarro.classicos),
           CarsListView(TipoCarro.esportivos),
           CarsListView(TipoCarro.luxo),
-        ],),
-        drawer: DrawerList(),
+        ],
       ),
+      drawer: DrawerList(),
     );
   }
 }
--- lib/pages/login/user.dart
+++ lib/pages/login/user.dart
@@ -1,20 +1,78 @@
+import 'package:cars/utils/prefs.dart';
+import 'dart:convert' as convert;
+
 class User {
   String login;
   String nome;
   String email;
+  String urlFoto;
   String token;
-
   List<String> roles;
 
-  User.fromJson(Map<String, dynamic> map)
-      : login = map["login"],
-        nome = map["nome"],
-        email = map["email"],
-        token = map["token"],
-        roles = map["roles"] != null ? map["roles"].map<String>((role) => role.toString()).toList() : null;
+  User(
+      {this.login,
+      this.nome,
+      this.email,
+      this.urlFoto,
+      this.token,
+      this.roles});
+
+  User.fromJson(Map<String, dynamic> json) {
+    login = json['login'];
+    nome = json['nome'];
+    email = json['email'];
+    urlFoto = json['urlFoto'];
+    token = json['token'];
+    roles = json['roles'].cast<String>();
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['login'] = this.login;
+    data['nome'] = this.nome;
+    data['email'] = this.email;
+    data['urlFoto'] = this.urlFoto;
+    data['token'] = this.token;
+    data['roles'] = this.roles;
+    return data;
+  }
+
+  void save() {
+    Map map = toJson();
+    String json = convert.json.encode(map);
+    Prefs.setString("user.prefs", json);
+  }
+
+  static Future<User> get() async {
+    String json = await Prefs.getString("user.prefs");
+    Map map = convert.json.decode(json);
+    User user = User.fromJson(map);
+    return user;
+  }
 
   @override
   String toString() {
-    return 'User{login: $login, nome: $nome, email: $email, token: $token, roles: $roles}';
+    return 'User{login: $login, nome: $nome, email: $email, urlFoto: $urlFoto, token: $token, roles: $roles}';
   }
 }
+
+// class User {
+//   String login;
+//   String nome;
+//   String email;
+//   String token;
+//
+//   List<String> roles;
+//
+//   User.fromJson(Map<String, dynamic> map)
+//       : login = map["login"],
+//         nome = map["nome"],
+//         email = map["email"],
+//         token = map["token"],
+//         roles = map["roles"] != null ? map["roles"].map<String>((role) => role.toString()).toList() : null;
+//
+//   @override
+//   String toString() {
+//     return 'User{login: $login, nome: $nome, email: $email, token: $token, roles: $roles}';
+//   }
+// }
